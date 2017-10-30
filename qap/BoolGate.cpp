e5518a548792f3ed13ca65cd7c8cce5312833f6c
Kunxian Xia <xiakunxian@onchain.com>
2017-10-30 18:57:34 +0800

    分析Bool门的QSP构造方案

--- qap/BoolGate.cpp
+++ qap/BoolGate.cpp
@@ -37,8 +37,23 @@ SparsePolyPtrVector* BoolGate::getOutput(bool a, bool b) {
 	return this->op(a, b) ? &output[0].one : &output[0].zero;
 }
 
+/*
+ *	gate checker function: 构造其对应的Quad Span Program S
+ *  S = (t(x), I_labeled = {I_l0, I_l1, I_r0, I_r1, I_o0, I_o1},
+ * 			 I_free = {})
+ * 
+ */ 
 void BoolGate::genericSP(QSP* qsp) {
-	int rootIndex = qsp->genFreshRoots(9, qsp->vTargetRoots);
+	/* 
+	 * [GGPR13]中的Lemma 2，构造12个向量，每个向量都在9维空间
+	 * 
+	 * 前9个向量是线性无关的，pi(ri) = 1 and pi(rj) = 0 (j \neq i)
+	 * 
+	 * 利用Lagrange插值法构造多项式，根据多项式在不同点的取值
+	 * 
+	 * pi至少是9次多项式
+	 */
+	int rootIndex = qsp->genFreshRoots(9, qsp->vTargetRoots); 
 	polys.reserve(9);
 	SparsePolynomial* sparsePoly;
 
@@ -51,13 +66,13 @@ void BoolGate::genericSP(QSP* qsp) {
 		sparsePoly->addNonZeroRoot(rootIndex + i, one, qsp->field);
 
 		if (i < 2) {
-			inputs[0]->zero.push_back(sparsePoly);
+			inputs[0]->zero.push_back(sparsePoly); //I_l0 = {p1, p2}
 		} else if (i < 4) {
-			inputs[0]->one.push_back(sparsePoly);
+			inputs[0]->one.push_back(sparsePoly); //I_l1 = {p3, p4}
 		} else if (i < 6) {
-			inputs[1]->zero.push_back(sparsePoly);
+			inputs[1]->zero.push_back(sparsePoly); //I_r0 = {p5, p6}
 		} else {
-			inputs[1]->one.push_back(sparsePoly);
+			inputs[1]->one.push_back(sparsePoly); //I_r1 = {p7, p8}
 		}
 	}
 
@@ -77,7 +92,7 @@ void BoolGate::genericSP(QSP* qsp) {
 	sparsePoly->addNonZeroRoot(rootIndex + 6, negOne, qsp->field);
 
 	// All output wires include the target's "special" 1 (target is (0, 0, ..., 0, 1))
-	sparsePoly->addNonZeroRoot(rootIndex + 8, one, qsp->field);
+	sparsePoly->addNonZeroRoot(rootIndex + 8, one, qsp->field); 
 
 	// Output wire 2
 	sparsePoly = qsp->newSparsePoly();
@@ -105,26 +120,26 @@ void BoolGate::genericSP(QSP* qsp) {
 
 	// Finally, extend the current v_0 with the _negative_ of the target vector for this SP (target is (0, 0, ..., 0, 1))
 	sparsePoly = qsp->getSPtarget();
-	sparsePoly->addNonZeroRoot(rootIndex + 8, negOne, qsp->field);
+	sparsePoly->addNonZeroRoot(rootIndex + 8, negOne, qsp->field);//*t
 	
 	// Assign the appropriate polynomials to each wire
 	assert(assignedInputs == 2);
 	if (inputs[0]->value && inputs[1]->value) {
-		inputs[0]->eval.push_back(polys[2]);
+		inputs[0]->eval.push_back(polys[2]); //l1, r1
 		inputs[1]->eval.push_back(polys[6]);
-		output->eval.push_back(polys[8]);
+		output->eval.push_back(polys[8]);  //t - l1 - r1
 	} else if (inputs[0]->value && !inputs[1]->value) {
-		inputs[0]->eval.push_back(polys[3]);
+		inputs[0]->eval.push_back(polys[3]); //l1', r0
 		inputs[1]->eval.push_back(polys[4]);
-		output->eval.push_back(polys[9]);
+		output->eval.push_back(polys[9]);  //t - l1' - r0
 	} else if (!inputs[0]->value && inputs[1]->value) {
-		inputs[0]->eval.push_back(polys[0]);
+		inputs[0]->eval.push_back(polys[0]); //l0, r1'
 		inputs[1]->eval.push_back(polys[7]);
-		output->eval.push_back(polys[10]);
+		output->eval.push_back(polys[10]); //t - l0 - r1'
 	} else if (!inputs[0]->value && !inputs[1]->value) {
-		inputs[0]->eval.push_back(polys[1]);
+		inputs[0]->eval.push_back(polys[1]); //l1, r0'
 		inputs[1]->eval.push_back(polys[5]);
-		output->eval.push_back(polys[11]);
+		output->eval.push_back(polys[11]); //t - l1 - r0'
 	} else {
 		assert(0);	// Shouldn't reach here
 	}
